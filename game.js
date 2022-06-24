3608d4035f9960f66bca336e9e5bc1f4a32f91cd
Cynthia <cynthiaphan98@gmail.com>
2022-06-24 14:30:28 -0700

    add game() and modify playRound()

--- game.js
+++ game.js
@@ -4,34 +4,65 @@ function computerPlay() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    playerSelection.toLowerCase();
+    computerSelection = computerPlay();
     const tie = "It's a tie! ";
     const win = "You win! ";
     const lose = "You lose! ";
     const declaration = ["Rock beats scissors.", "Paper beats rock.", "Scissors beat paper."];
-    if (playerSelection === computerSelection) {
-        return tie;
+    if (playerSelection == computerSelection) {
+        console.log(tie);
     } else if (playerSelection == "rock") {
         if (computerSelection == "paper") {
-            return lose + declaration[1];
+            console.log(lose + declaration[1]);
+            return lose;
         } else {
-            return win + declaration[0];
+            console.log(win + declaration[0]);
+            return win;
         }
     } else if (playerSelection == "paper") {
         if (computerSelection == "rock") {
-            return win + declaration[1];
+            console.log(win + declaration[1]);
+            return win;
         } else {
-            return lose + declaration[2];
+            console.log(lose + declaration[2]);
+            return lose;
         }
     } else {
         if (computerSelection == "rock") {
-            return lose + declaration[0];
+            console.log(lose + declaration[0]);
+            return lose;
         } else {
-            return win + declaration[2];
+            console.log(win + declaration[2]);
+            return win;
         }
     }
 }
 
-const playerSelection = prompt("choose your weapon", "");
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection))
+
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    let playerSelection;
+    let computerSelection;
+    for (let i = 0; i < 5; i++) {
+        playerSelection = prompt("Choose your weapon.", "");
+        playerSelection.toLowerCase();
+        result = playRound(playerSelection, computerSelection);
+        if (result == "You win! ") {
+            console.log(`You ${++playerScore} - Computer ${computerScore}`);
+        } else if (result == "You lose! ") {
+            console.log(`You ${playerScore} - Computer ${++computerScore}`);
+        } else {
+            console.log(`You ${playerScore} - Computer ${computerScore}`);
+        }
+    }
+    if (playerScore > computerScore) {
+        console.log("YAY! You win!");
+    } else if (playerScore < computerScore) {
+        console.log("You lose. Try again!");
+    } else {
+        console.log("It's a tie!");
+    }
+}
+
+game();
